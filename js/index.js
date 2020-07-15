cdae716cd631637ad8993f6b73b912b4ef1fac15
Hamida Sharifova <hamida@Hamidas-MacBook-Pro.local>
2020-07-14 19:23:45 -0700

    MVP tasks, propogation and setdefault

--- js/index.js
+++ js/index.js
@@ -13,11 +13,15 @@ logo.addEventListener('dblclick',()=>{
 
 //2. Change each anchor's color, when there is a click
 
-const navAnchor = document.querySelectorAll(".nav-link");
-navAnchor.forEach((item) => {
-    item.addEventListener('click',(event)=>{
-        item.style.color = 'purple';
-    })
+const navAnchor = document.querySelector(".nav");
+// navAnchor.forEach((item) => {
+//     item.addEventListener('click',(event)=>{
+//         item.style.color = 'purple';
+//     })
+// })
+
+navAnchor.addEventListener('click',(event) => {
+    event.target.style.color = 'purple';
 })
 
 //3. Header logo back to normal on mouseleave
@@ -31,4 +35,51 @@ logo.addEventListener('mouseleave', (event) => {
 });
 
 
+// 4. MouseOver color change for anchors
+navAnchor.addEventListener('mouseover', (event)=>{
+    event.target.style.color = "red";
+    setTimeout(()=>{
+        event.target.style.color = "";
+    },500);
+}, false);
 
+// 5. Resize to alert window size 
+
+    let height = window.innerHeight;
+    let width = window.innerWidth;
+    window.addEventListener('resize',()=> {
+        while(width < 400){
+            alert(`Small screen is detected. Screen size is: ${height} by ${width}`);
+        }  
+    })
+
+//6.contextMenu (right click) on images to display their aleternative names
+
+let images = document.querySelectorAll('img');
+
+images.forEach((item) => {
+    item.addEventListener('contextmenu',()=>{
+        alert(`${item.alt} is protected by copyright`);
+    })
+//7. Use preventDefault to prevent dragging for images
+    item.addEventListener('dragstart',(event)=>{
+        event.preventDefault();
+    })
+})
+
+//event Propogation
+
+    let intro = document.querySelector('.intro');
+    console.log(intro);
+    intro.addEventListener('dblclick',() => {
+        intro.style.border = '2px solid black';
+        intro.style.borderRadius = '20px';
+    })
+
+    let introP = document.querySelector('.intro p');
+    console.log(introP);
+
+    introP.addEventListener('dblclick', (event)=>{
+        event.stopPropagation();
+        introP.style.color = 'blue';
+    })
